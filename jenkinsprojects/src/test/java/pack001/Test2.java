f2e8c943fe45addf5e116284af093d435d2337e2
Sohail7984 <Sksohail7984@gmail.com>
2025-10-28 12:34:21 +0530

    Fixed imports and added Selenium dependencies in Test2.java

--- jenkinsprojects/src/test/java/pack001/Test2.java
+++ jenkinsprojects/src/test/java/pack001/Test2.java
@@ -1,16 +1,17 @@
 package pack001;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class TestJenkins {
+public class Test2 {
   @Test
   public void test001() throws Exception {
-	  ChromeOptions options = new chromeOptions();
-    WebDriver driver = new RemoteDriver(new URL("http://localhost:4444/wd/hub"),options);
+	  ChromeOptions options = new ChromeOptions();
+	  WebDriver driver = new RemoteWebDriver (new URL("http://localhost:4444/wd/hub"),options);
 	  Thread.sleep(2000);
 	  driver.manage().window().maximize();
 	  Thread.sleep(2000);
